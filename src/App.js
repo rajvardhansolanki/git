6bbba8921f60dc97de26096ecf6d9a874d61bee2
Rajvardhan solanki <rajvardhansolanki0@gmail.com>
2022-07-21 11:54:30 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css'
+import StateTest from './tests/state/stateTest';
+import Apimock from './tests/api testing/apimock';
+
+
+function App() {
+  return (
+    <>
+      {/* <StateTest /> */}
+      <Apimock />
+    </>
+  );
+}
+
+export default App;
